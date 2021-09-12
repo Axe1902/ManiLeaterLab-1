6826dfec3e24b804e752a913da2669cbf38f23f0
Axe1902 <nikita-bar@bk.ru>
2021-09-12 21:59:02 +0300

    Number5-second commit

--- Number5/Number5/Number5.cpp
+++ Number5/Number5/Number5.cpp
@@ -11,7 +11,7 @@ void main() {
 	int i;
 
 	struct Students {
-		char Name[20], Surname[20];
+		char Name[20], Surname[20], Facult[10];
 		int NumberOfBook;
 	} stud[4];
 
@@ -20,29 +20,31 @@ void main() {
 		cin >> stud[i].Name;
 		cout << "Введите фамилию студента: ";
 		cin >> stud[i].Surname;
+		cout << "Введите факультет студента: ";
+		cin >> stud[i].Facult;
 		cout << "Введите номер зачетной книжки: ";
 		cin >> stud[i].NumberOfBook;
 		cout << endl;
 	}
 
-
 	for (i = 0; i < 4; i++) {
-		printf("|%10s|%10s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].NumberOfBook);
+		printf("|%10s|%10s|%5s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].Facult, stud[i].NumberOfBook);
 	}
 	Sleep(3000);
 
 	bool running = true;
-	char name[20], surname[20];
+	char name[20], surname[20], facult[10];
 	char choise;
 	int number, CounterOfExist;
 
 	while (running){
+
 		system("cls");
 
 		cout << "По какому параметру производить поиск?" << endl;
-		cout << " 1. По имени\n 2. По фамилии\n 3. По имени и фамилии\n 4. По имени или фамилии\n 5. По номеру зачетной книжки\n" << endl;
+		cout << " 1. По имени\n 2. По фамилии\n 3. По имени и фамилии\n 4. По имени или фамилии\n 5. По факультету\n 6. По факультету и имени\n 7. По факультету и фамилии\n 8. По номеру зачетной книжки\n" << endl;
 
-		cout << "Для выхода из программы введите 6" << endl;
+		cout << "Для выхода из программы введите 9" << endl;
 		cin >> choise;
 
 		switch (choise) {
@@ -54,7 +56,7 @@ void main() {
 			cout << "\nСтудент/студенты с заданными параметрами: \n";
 			for (i = 0; i < 4; i++) {
 				if (strcmp(name, stud[i].Name) == 0) {
-					printf("|%10s|%10s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].NumberOfBook);
+					printf("|%10s|%10s|%5s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].Facult, stud[i].NumberOfBook);
 					CounterOfExist++;
 				}
 			} 
@@ -70,7 +72,7 @@ void main() {
 			cout << "\nСтудент/студенты с заданными параметрами: \n";
 			for (i = 0; i < 4; i++) {
 				if (strcmp(surname, stud[i].Surname) == 0) {
-					printf("|%10s|%10s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].NumberOfBook);
+					printf("|%10s|%10s|%5s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].Facult, stud[i].NumberOfBook);
 					CounterOfExist++;
 				} 
 			} 
@@ -88,7 +90,7 @@ void main() {
 			cout << "\nСтудент/студенты с заданными параметрами: \n";
 			for (i = 0; i < 4; i++) {
 				if ((strcmp(name, stud[i].Name) == 0) && (strcmp(surname, stud[i].Surname) == 0)) {
-					printf("|%10s|%10s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].NumberOfBook);
+					printf("|%10s|%10s|%5s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].Facult, stud[i].NumberOfBook);
 					CounterOfExist++;
 				}
 			} 
@@ -106,7 +108,7 @@ void main() {
 			cout << "\nСтудент/студенты с заданными параметрами: \n";
 			for (i = 0; i < 4; i++) {
 				if ((strcmp(name, stud[i].Name) == 0) || (strcmp(surname, stud[i].Surname) == 0)) {
-					printf("|%10s|%10s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].NumberOfBook);
+					printf("|%10s|%10s|%5s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].Facult, stud[i].NumberOfBook);
 					CounterOfExist++;
 				}
 			} 
@@ -116,13 +118,65 @@ void main() {
 
 		case '5':
 			CounterOfExist = 0;
+			cout << "Введите факультет: ";
+			cin >> facult;
+
+			cout << "\nСтудент/студенты с заданными параметрами: \n";
+			for (i = 0; i < 4; i++) {
+				if (strcmp(facult, stud[i].Facult) == 0) {
+					printf("|%10s|%10s|%5s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].Facult, stud[i].NumberOfBook);
+					CounterOfExist++;
+				}
+			}
+			if (CounterOfExist == 0) cout << "Не существует";
+			Sleep(3000);
+			break;
+
+		case '6':
+			CounterOfExist = 0;
+			cout << "Введите факультет: ";
+			cin >> facult;
+			cout << "Введите имя: ";
+			cin >> name;
+
+			cout << "\nСтудент/студенты с заданными параметрами: \n";
+			for (i = 0; i < 4; i++) {
+				if ((strcmp(facult, stud[i].Facult) == 0) && (strcmp(name, stud[i].Name) == 0)) {
+					printf("|%10s|%10s|%5s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].Facult, stud[i].NumberOfBook);
+					CounterOfExist++;
+				}
+			}
+			if (CounterOfExist == 0) cout << "Не существует";
+			Sleep(3000);
+			break;
+
+		case '7':
+			CounterOfExist = 0;
+			cout << "Введите факультет: ";
+			cin >> facult;
+			cout << "Введите фамилию: ";
+			cin >> surname;
+
+			cout << "\nСтудент/студенты с заданными параметрами: \n";
+			for (i = 0; i < 4; i++) {
+				if ((strcmp(facult, stud[i].Facult) == 0) && (strcmp(surname, stud[i].Surname) == 0)) {
+					printf("|%10s|%10s|%5s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].Facult, stud[i].NumberOfBook);
+					CounterOfExist++;
+				}
+			}
+			if (CounterOfExist == 0) cout << "Не существует";
+			Sleep(3000);
+			break;
+
+		case '8':
+			CounterOfExist = 0;
 			cout << "Введите номер зачетной книжки: ";
 			cin >> number;
 
 			cout << "\nСтудент/студенты с заданными параметрами: \n";
 			for (i = 0; i < 4; i++) {
 				if (number == stud[i].NumberOfBook) {
-					printf("|%10s|%10s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].NumberOfBook);
+					printf("|%10s|%10s|%5s|%5d|\n", stud[i].Surname, stud[i].Name, stud[i].Facult, stud[i].NumberOfBook);
 					CounterOfExist++;
 				}
 			} 
@@ -130,7 +184,7 @@ void main() {
 			Sleep(3000);
 			break;
 
-		case '6':
+		case '9':
 			running = false; 
 			break;
 
